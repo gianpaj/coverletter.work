dcead422159cf324ba0f3caa2638be946c5ce88a
Gianfranco Palumbo <gianpa@gmail.com>
2024-11-04 22:38:01 +0100

    not banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,19 @@
 import { AI } from '@/lib/chat/actions';
 import { generateUUID } from '@/lib/utils';
 import Landing from '@/components/landing/Landing';
+// import { Banner } from '@/components/ui/banner';
 
 export default function IndexPage() {
   const id = generateUUID();
 
   return (
-    <div className="px-2 sm:px-0">
-      <AI initialAIState={{ coverLetterId: id, messages: [] }}>
-        <Landing />
-      </AI>
-    </div>
+    <>
+      {/* <Banner /> */}
+      <div className="px-2 sm:px-0">
+        <AI initialAIState={{ coverLetterId: id, messages: [] }}>
+          <Landing />
+        </AI>
+      </div>
+    </>
   );
 }
---
+++ components/ui/banner.tsx
@@ -0,0 +1,15 @@
+export const Banner = () => (
+  <div className="bg-indigo-600">
+    <div className="mx-auto max-w-screen-xl items-center justify-center gap-x-4 px-4 py-3 text-white sm:flex md:px-8">
+      <p className="py-2 font-medium">
+        We just launched our new product! you can check out all features now.
+      </p>
+      <a
+        href="javascript:void(0)"
+        className="mt-3 inline-block w-full flex-none rounded-lg bg-white px-3 py-2 text-center font-medium text-indigo-600 duration-150 hover:bg-gray-100 active:bg-gray-200 sm:mt-0 sm:w-auto sm:text-sm"
+      >
+        Learn more
+      </a>
+    </div>
+  </div>
+);
